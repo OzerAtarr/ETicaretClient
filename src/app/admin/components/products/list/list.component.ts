588f8f5778d057e3706d9c933077fb939649261b
90536 <90536@DESKTOP-FCHRJ42>
2024-01-23 21:56:45 +0300

    33

--- src/app/admin/components/products/list/list.component.ts
+++ src/app/admin/components/products/list/list.component.ts
@@ -6,6 +6,8 @@ import { BaseComponent, SpinnerType } from '../../../../base/base.component';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { AlertifyService, MessageType, Position } from '../../../../services/admin/alertify.service';
 import { MatPaginator } from '@angular/material/paginator';
+import { DialogService } from '../../../../services/common/dialog.service';
+import { SelectProductImageDialogComponent } from '../../../../dialogs/select-product-image-dialog/select-product-image-dialog.component';
 declare var $: any;
 
 @Component({
@@ -14,11 +16,12 @@ declare var $: any;
   styleUrl: './list.component.scss'
 })
 export class ListComponent extends BaseComponent implements OnInit{
-  constructor(spinner: NgxSpinnerService, private poductService: ProductService, private alertifyService: AlertifyService) {
+  constructor(spinner: NgxSpinnerService, private poductService: ProductService, private alertifyService: AlertifyService,
+    private dialogService: DialogService) {
     super(spinner)
   }
 
-  displayedColumns: string[] = ['name', 'stock', 'price', 'createdDate', 'updatedDate', 'edit', 'delete'];
+  displayedColumns: string[] = ['name', 'stock', 'price', 'createdDate', 'updatedDate', 'photos', 'edit', 'delete'];
   dataSource: MatTableDataSource<List_Product> = null;
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
@@ -38,6 +41,16 @@ export class ListComponent extends BaseComponent implements OnInit{
   //   const img: HTMLImageElement = event.srcElement;
   //   $(img.parentElement.parentElement).fadeOut(2000);
   // }
+
+  addProductImages(id: string){
+    this.dialogService.openDialog({
+      componentTpye: SelectProductImageDialogComponent,
+      data: id,
+      options: {
+        width: "1400px"
+      }
+    });
+  }
 
   async pageChanged(){
     await this.getProducts();
--- src/app/services/common/models/product.service.ts
+++ src/app/services/common/models/product.service.ts
@@ -5,6 +5,7 @@ import { Injectable } from '@angular/core';
 import { HttpErrorResponse } from '@angular/common/http';
 import { List_Product } from '../../../contracts/list_product';
 import { Observable, firstValueFrom } from 'rxjs';
+import { List_Product_Image } from '../../../contracts/list_product_image';
 
 @Injectable({
   providedIn: 'root'
@@ -48,5 +49,27 @@ export class ProductService {
     }, id);
 
     await firstValueFrom(deleteObservable);
+  }
+
+  async readImages(id: string,  successCallBack?: ()=> void): Promise<List_Product_Image[]>{
+    const getObsorvable: Observable<List_Product_Image[]> = this.httpClientService.get<List_Product_Image[]>({
+      action: "getproductimages",
+      controller: "products"
+    }, id);
+
+    const images: List_Product_Image[] = await firstValueFrom(getObsorvable)
+    successCallBack();
+    return images;
+  }
+
+
+  async deleteImage(id: string, imageId: string, successCallBack?: ()=> void){
+      const deleteObservable = this.httpClientService.delete({
+        action: "deleteproductimage",
+        controller: "products",
+        queryString: `imageId=${imageId}`
+      }, id);
+      await firstValueFrom(deleteObservable);
+      successCallBack();
   }
 }
